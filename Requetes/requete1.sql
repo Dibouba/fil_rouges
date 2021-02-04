88cd02a9946a2e272ec2d9ef28cd079043735769
Dibouba <diallobouba905@gmail.com>
2021-02-04 23:58:56 +0100

    commit des requetes de fil rouges

---
+++ Requetes/requete1.sql
@@ -0,0 +1,3 @@
+/*la nouvelle ligne de la table commande*/
+insert into `commandes`(`com_date`,`com_date_paie`,`com_livr`,`com_exp`,`com_cli_id`)
+values('2020-06-28','2020-06-28','2020-06-29','2020-06-30',10);
